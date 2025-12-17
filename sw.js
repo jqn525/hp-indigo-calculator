a22fc68e0dbbd13b727ac5dff3aa9c868aca12c7
Jonathan <jonathan.nguyen@me.com>
2025-12-17 15:42:35 -0800

    Bump cache to v199

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sfu-calc-v198';
+const CACHE_NAME = 'sfu-calc-v199';
 
 // Determine hosting environment and set appropriate base path
 const isLocalhost = self.location.hostname === 'localhost' || self.location.hostname === '127.0.0.1';
